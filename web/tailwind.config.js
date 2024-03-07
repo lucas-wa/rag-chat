b8d5ed0afc0222f4f713718f5bbc1617804c6b7d
lucas-wa <lucaswanderley600@gmail.com>
2024-03-07 22:52:25 +0000

    Fixing Web

---
+++ web/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+      // Annimation for lateral menu
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slideOut: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+
+      animation: {
+        slideIn: 'slideIn 0.5s ease-in-out',
+        slideOut: 'slideOut 0.5s ease-in-out',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
